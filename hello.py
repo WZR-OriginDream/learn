231f378d822baea962e9a3716bff51bbbf23eca4
wen <2195322633@qq.com>
2022-12-04 23:04:34 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+"""
+@Author:wzr
+@Description:TODO
+@Create:2022/12/4 - 22:52
+@Version:v1.0
+"""
+
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy;
 
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -17,3 +18,10 @@ if __name__ == '__main__':
     hello('hello word!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    hello('hello word!')
+    print_hi('PyCharm')
+    hello('hello word!')
+    print('Hello')
